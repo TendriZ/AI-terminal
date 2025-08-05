668afb84a5c84a2eeeb24fea694164756eab7c7f
TendriZ <rakarazzani24@gmail.com>
2025-08-05 16:55:14 +0700

    modified the boolean

--- AI.py
+++ AI.py
@@ -6,13 +6,8 @@ load_dotenv()
 
 API_KEY = os.getenv("API_KEY") 
 URL = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key={API_KEY}" 
-INSTRUCTION = (
-    "Berikan jawaban yang lengkap dan mendalam. Jelaskan langkah-langkahnya dengan jelas, "
-    "gunakan contoh jika memungkinkan. Jangan terlalu singkat. Jelaskan seolah-olah kepada pemula.\n\n"
-)
-
 def chatbotGemini(prompt):
-    full_prompt = INSTRUCTION + prompt
+    full_prompt = prompt
     obj_data = {
         "contents": [{
             "parts": [{
@@ -35,11 +30,12 @@ def chatbotGemini(prompt):
         print(f"[⚠️] Error {res.status_code}: {res.text}")
 
 print("[AI] > Halo! Aku siap membantu. (Ketik 'keluar' untuk berhenti)\n")
-
-while True:
+status = True
+while status == True:
     user = input("[kamu] > ")
     if user.lower() == "keluar":
         print("[AI] > Sampai jumpa ya!")
-        break
+        exit()
     else:
         chatbotGemini(user)
+        status = True
