1f328d22500138d7644d0bd79387fba58ab7d9e0
TendriZ <rakarazzani24@gmail.com>
2025-08-05 20:02:37 +0700

    add backend url

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const chat = document.getElementById("chat");
 
 function sendPrompt() {
   const prompt = input.value;
-  fetch("https://chatbot-gemini.rakadev.repl.co", {
+  fetch("https://39ce7961-2181-494f-95cd-cca1e2dd7af1-00-k7zwjwdwq87j.pike.replit.dev/", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ prompt })
